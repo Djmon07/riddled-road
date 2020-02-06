06b90ccf940dbcf732b6e042c53d2bbbbc280e7f
Daniel Monville <djmonwork@gmail.com>
2020-02-06 16:23:31 -0500

    Added comments into code.

--- engine.py
+++ engine.py
@@ -3,13 +3,16 @@ import scene
 class Engine(object):
 
     def __init__(self, scene):
+        #class in map class
         self.scene = scene
 
     def run(self):
+        #gets class from map
         current_scene = self.scene.opening_scene()
         final = self.scene.next_scene('finished')
 
         while current_scene != final:
+            #checks for win/ loss condition
             next_scene_name = current_scene.enter()
             current_scene = self.scene.next_scene(next_scene_name)
 
--- map.py
+++ map.py
@@ -11,11 +11,14 @@ class Map(object):
         'death': scene.Death(),
         'finished': scene.Finished()
     }
+    #scenes used in game
 
     def __init__(self, start_scene):
+        #used to hold the scene
         self.start_scene = start_scene
 
     def next_scene(self, scene_name):
+        #used to hold the upcoming scene
         new_scene = Map.scene.get(scene_name)
         return new_scene
 
